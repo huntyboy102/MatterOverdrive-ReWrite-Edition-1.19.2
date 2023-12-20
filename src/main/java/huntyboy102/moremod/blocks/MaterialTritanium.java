a3072813f0c28fab79cd820bfed95479d4058ab3
huntyboy102 <hrlovering@hotmail.com>
2023-12-20 17:07:53 -0500

    FIXED TRITANIUM

--- src/main/java/huntyboy102/moremod/blocks/MaterialTritanium.java
+++ src/main/java/huntyboy102/moremod/blocks/MaterialTritanium.java
@@ -1,14 +1,14 @@
 
 package huntyboy102.moremod.blocks;
 
-import net.minecraft.block.material.MapColor;
-import net.minecraft.block.material.Material;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 
-public class MaterialTritanium extends Material {
+public class MaterialTritanium extends Block {
 
-	public MaterialTritanium(MaterialColor color) {
-		super(color);
-		setRequiresTool();
+	public MaterialTritanium() {
+		super(Properties.of(Material.STONE, MaterialColor.COLOR_GRAY)
+				.requiresCorrectToolForDrops());
 	}
 }
