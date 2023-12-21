69a42557c97d43883b3d3520f15807f5f1930469
huntyboy102 <hrlovering@hotmail.com>
2023-12-20 23:36:49 -0500

    Update MatterOverdriveRecipes.java

--- src/main/java/huntyboy102/moremod/init/MatterOverdriveRecipes.java
+++ src/main/java/huntyboy102/moremod/init/MatterOverdriveRecipes.java
@@ -5,23 +5,38 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import org.apache.commons.io.IOUtils;
-
 import huntyboy102.moremod.MatterOverdriveRewriteEdition;
 import huntyboy102.moremod.data.recipes.InscriberRecipeManager;
+import huntyboy102.moremod.Reference;
+import net.minecraft.world.item.crafting.CampfireCookingRecipe;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraftforge.registries.DeferredRegister;
+
+
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraft.world.item.crafting.SimpleCookingSerializer;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class MatterOverdriveRecipes {
-	public static final InscriberRecipeManager INSCRIBER = new InscriberRecipeManager();
+	public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, Reference.MOD_ID);
+
+	public static final RegistryObject<RecipeSerializer<CampfireCookingRecipe>> TRITANIUM_SMELTING = RECIPE_SERIALIZERS.register("tritanium_smelting",
+			() -> new SimpleCookingSerializer<>(CampfireCookingRecipe::new, 200));
+	public static final RegistryObject<RecipeSerializer<CampfireCookingRecipe>> TRITANIUM_ORE_SMELTING = RECIPE_SERIALIZERS.register("tritanium_ore_smelting",
+			() -> new SimpleCookingSerializer<>(CampfireCookingRecipe::new, 200));
+
+
+	//public static final InscriberRecipeManager INSCRIBER = new InscriberRecipeManager();
 
 	public static void registerMachineRecipes() {
 
 		// Furnace
-		GameRegistry.addSmelting(new ItemStack(MatterOverdriveRewriteEdition.ITEMS.tritanium_dust),
-				new ItemStack(MatterOverdriveRewriteEdition.ITEMS.tritanium_ingot), 5);
-		GameRegistry.addSmelting(new ItemStack(MatterOverdriveRewriteEdition.BLOCKS.tritaniumOre),
-				new ItemStack(MatterOverdriveRewriteEdition.ITEMS.tritanium_ingot), 10);
+		RecipeSerializer<CampfireCookingRecipe> tritaniumSmelting = TRITANIUM_SMELTING.get();
+		ForgeRegistries.RECIPE_SERIALIZERS.register("tritanium_smelting", () -> tritaniumSmelting);
+
+		RecipeSerializer<CampfireCookingRecipe> tritaniumOreSmelting = TRITANIUM_ORE_SMELTING.get();
+		ForgeRegistries.RECIPE_SERIALIZERS.register("tritanium_ore_smelting", () -> tritaniumOreSmelting);
 
 		// Inscriber
 		File file = new File(MatterOverdriveRewriteEdition.CONFIG_HANDLER.configDir, "MatterOverdrive/recipes/inscriber.xml");
