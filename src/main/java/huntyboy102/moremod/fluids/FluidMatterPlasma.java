de796fce08286c60922e1c2b679953d12930fe31
huntyboy102 <hrlovering@hotmail.com>
2023-12-28 13:37:38 -0500

    Fluids have been dealt with...

--- src/main/java/huntyboy102/moremod/fluids/FluidMatterPlasma.java
+++ src/main/java/huntyboy102/moremod/fluids/FluidMatterPlasma.java
@@ -1,18 +1,19 @@
-
 package huntyboy102.moremod.fluids;
 
 import huntyboy102.moremod.Reference;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.material.Fluids;
-import net.minecraftforge.fluids.ForgeFlowingFluid;
+import net.minecraftforge.fluids.FluidType;
 
-public class FluidMatterPlasma extends ForgeFlowingFluid {
+public class FluidMatterPlasma extends FluidType {
+	public static final ResourceLocation STILL_TEXTURE = new ResourceLocation(Reference.MOD_ID, "fluids/matter_plasma/still");
+	public static final ResourceLocation FLOWING_TEXTURE = new ResourceLocation(Reference.MOD_ID, "fluids/matter_plasma/flowing");
 	public FluidMatterPlasma() {
-		super(new Properties(() -> Fluids.EMPTY, () -> Fluids.EMPTY,
-				new ResourceLocation(Reference.MOD_ID, "fluids/matter_plasma/still"),
-				new ResourceLocation(Reference.MOD_ID, "fluids/matter_plasma/flowing"))
+		super(Properties.create()
 				.density(8000)
 				.viscosity(8000)
-				.luminosity(15));
+				.lightLevel(15)
+		);
 	}
 }
+
+
--- src/main/java/huntyboy102/moremod/fluids/FluidMoltenTritanium.java
+++ src/main/java/huntyboy102/moremod/fluids/FluidMoltenTritanium.java
@@ -2,15 +2,18 @@
 package huntyboy102.moremod.fluids;
 
 import huntyboy102.moremod.Reference;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fluids.Fluid;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fluids.FluidType;
 
-public class FluidMoltenTritanium extends Fluid {
+public class FluidMoltenTritanium extends FluidType {
+	public static final ResourceLocation STILL_TEXTURE = new ResourceLocation(Reference.MOD_ID, "fluids/molten_tritanium/still");
+	public static final ResourceLocation FLOWING_TEXTURE = new ResourceLocation(Reference.MOD_ID, "fluids/molten_tritanium/flowing");
+
 	public FluidMoltenTritanium(String fluidName) {
-		super(fluidName, new ResourceLocation(Reference.MOD_ID, "fluids/molten_tritanium/still"),
-				new ResourceLocation(Reference.MOD_ID, "fluids/molten_tritanium/flowing"));
-		setViscosity(6000);
-		setLuminosity(15);
-		setTemperature(2000);
+        super(Properties.create()
+				.viscosity(6000)
+				.lightLevel(15)
+				.temperature(2000)
+		);
 	}
 }
