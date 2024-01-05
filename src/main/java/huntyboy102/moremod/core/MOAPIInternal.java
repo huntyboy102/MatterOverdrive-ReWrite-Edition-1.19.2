06a2f6d89d83523fcd4aecf5e0e9660f53a54b85
huntyboy102 <hrlovering@hotmail.com>
2024-01-05 09:20:32 -0500

    Update MOAPIInternal.java

--- src/main/java/huntyboy102/moremod/core/MOAPIInternal.java
+++ src/main/java/huntyboy102/moremod/core/MOAPIInternal.java
@@ -2,49 +2,49 @@
 package huntyboy102.moremod.core;
 
 import huntyboy102.moremod.proxy.ClientProxy;
-import matteroverdrive.MatterOverdrive;
+import huntyboy102.moremod.MatterOverdriveRewriteEdition;
 import huntyboy102.moremod.api.MatterOverdriveAPI;
-import matteroverdrive.api.android.IAndroidStatRegistry;
-import matteroverdrive.api.android.IAndroidStatRenderRegistry;
+import huntyboy102.moremod.api.android.IAndroidStatRegistry;
+import huntyboy102.moremod.api.android.IAndroidStatRenderRegistry;
 import huntyboy102.moremod.api.dialog.IDialogRegistry;
 import huntyboy102.moremod.api.matter.IMatterRegistry;
 import huntyboy102.moremod.api.renderer.IBionicPartRenderRegistry;
 import huntyboy102.moremod.api.starmap.IStarmapRenderRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class MOAPIInternal implements MatterOverdriveAPI {
 	public static final MOAPIInternal INSTANCE = new MOAPIInternal();
 
 	@Override
 	public IMatterRegistry getMatterRegistry() {
-		return MatterOverdrive.MATTER_REGISTRY;
+		return MatterOverdriveRewriteEdition.MATTER_REGISTRY;
 	}
 
 	@Override
 	public IAndroidStatRegistry getAndroidStatRegistry() {
-		return MatterOverdrive.STAT_REGISTRY;
+		return MatterOverdriveRewriteEdition.STAT_REGISTRY;
 	}
 
 	@Override
 	public IDialogRegistry getDialogRegistry() {
-		return MatterOverdrive.DIALOG_REGISTRY;
+		return MatterOverdriveRewriteEdition.DIALOG_REGISTRY;
 	}
 
 	@Override
-	@SideOnly(Side.CLIENT)
+	@OnlyIn(Dist.CLIENT)
 	public IAndroidStatRenderRegistry getAndroidStatRenderRegistry() {
 		return ClientProxy.renderHandler.getStatRenderRegistry();
 	}
 
 	@Override
-	@SideOnly(Side.CLIENT)
+	@OnlyIn(Dist.CLIENT)
 	public IBionicPartRenderRegistry getBionicStatRenderRegistry() {
 		return ClientProxy.renderHandler.getBionicPartRenderRegistry();
 	}
 
 	@Override
-	@SideOnly(Side.CLIENT)
+	@OnlyIn(Dist.CLIENT)
 	public IStarmapRenderRegistry getStarmapRenderRegistry() {
 		return ClientProxy.renderHandler.getStarmapRenderRegistry();
 	}
