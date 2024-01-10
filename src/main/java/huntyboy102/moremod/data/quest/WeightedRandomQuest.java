03678fbef24fa9b090bfded5286b71f012be28cd
huntyboy102 <hrlovering@hotmail.com>
2024-01-10 09:53:04 -0500

    Updated WeightedRandom Classes

--- src/main/java/huntyboy102/moremod/data/quest/WeightedRandomQuest.java
+++ src/main/java/huntyboy102/moremod/data/quest/WeightedRandomQuest.java
@@ -2,17 +2,22 @@
 package huntyboy102.moremod.data.quest;
 
 import huntyboy102.moremod.api.quest.Quest;
-import net.minecraft.util.WeightedRandom;
+import net.minecraft.util.random.WeightedRandom;
 
-public class WeightedRandomQuest extends WeightedRandom.Item {
+public class WeightedRandomQuest {
 	final Quest quest;
+	final int weight;
 
 	public WeightedRandomQuest(Quest quest, int weight) {
-		super(weight);
 		this.quest = quest;
+		this.weight = weight;
 	}
 
 	public Quest getQuest() {
 		return quest;
+	}
+
+	public int getWeight() {
+		return weight;
 	}
 }
