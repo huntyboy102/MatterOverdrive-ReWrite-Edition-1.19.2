f6ff9c94214ada3def7719fe8c42424e9651a4cb
huntyboy102 <hrlovering@hotmail.com>
2024-01-10 11:26:21 -0500

    Update CommonProxy.java

--- src/main/java/huntyboy102/moremod/proxy/CommonProxy.java
+++ src/main/java/huntyboy102/moremod/proxy/CommonProxy.java
@@ -2,16 +2,14 @@
 package huntyboy102.moremod.proxy;
 
 import huntyboy102.moremod.compat.MatterOverdriveCompat;
-import matteroverdrive.MatterOverdrive;
+import huntyboy102.moremod.MatterOverdriveRewriteEdition;
 import huntyboy102.moremod.handler.weapon.CommonWeaponHandler;
 import huntyboy102.moremod.starmap.GalaxyServer;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.text.translation.I18n;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.network.NetworkEvent;
 
 public class CommonProxy {
 	private final CommonWeaponHandler commonWeaponHandler;
@@ -24,23 +22,23 @@ public class CommonProxy {
 		MatterOverdriveCompat.registerModules();
 	}
 
-	public EntityPlayer getPlayerEntity(MessageContext ctx) {
-		return ctx.getServerHandler().player;
+	public Player getPlayerEntity(NetworkEvent.Context ctx) {
+		return ctx.getSender();
 	}
 
-	public void preInit(FMLPreInitializationEvent event) {
+	public void preInit(FMLCommonSetupEvent event) {
 		registerCompatModules();
 	}
 
-	public void init(FMLInitializationEvent event) {
+	public void init(FMLCommonSetupEvent event) {
 		MinecraftForge.EVENT_BUS.register(GalaxyServer.getInstance());
 		MinecraftForge.EVENT_BUS.register(getWeaponHandler());
 		MinecraftForge.EVENT_BUS.register(GalaxyServer.getInstance());
-		MatterOverdrive.CONFIG_HANDLER.subscribe(GalaxyServer.getInstance());
-		MatterOverdrive.CONFIG_HANDLER.subscribe(GalaxyServer.getInstance().getGalaxyGenerator());
+		MatterOverdriveRewriteEdition.CONFIG_HANDLER.subscribe(GalaxyServer.getInstance());
+		MatterOverdriveRewriteEdition.CONFIG_HANDLER.subscribe(GalaxyServer.getInstance().getGalaxyGenerator());
 	}
 
-	public void postInit(FMLPostInitializationEvent event) {
+	public void postInit(FMLCommonSetupEvent event) {
 	}
 
 	public CommonWeaponHandler getWeaponHandler() {
@@ -48,11 +46,11 @@ public class CommonProxy {
 	}
 
 	public boolean hasTranslation(String key) {
-		return I18n.canTranslate(key);
+		return I18n.exists(key);
 	}
 
 	public String translateToLocal(String key, Object... params) {
-		return I18n.translateToLocalFormatted(key, params);
+		return I18n.get(key, params);
 	}
 
 	public void matterToast(boolean b, long l) {
